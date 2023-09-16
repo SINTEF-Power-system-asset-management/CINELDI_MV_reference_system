739a77ee1d4c5f334baeb9aca7e1b07e4ca6122e
Iver Bakken Sperstad <iver.bakken.sperstad@sintef.no>
2023-09-16 16:43:35 +0200

    Complete intro script for exercise 2

--- exercise_2_load_analysis.py
+++ exercise_2_load_analysis.py
@@ -21,6 +21,7 @@ import load_profiles as lp
 import pandapower_read_csv as ppcsv
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 
 # %% Define input data
@@ -40,23 +41,40 @@ filename_load_data_fullpath = os.path.join(path_data_set,'load_data_CINELDI_MV_r
 filename_load_mapping_fullpath = os.path.join(path_data_set,'mapping_loads_to_CINELDI_MV_reference_grid.csv')
 filename_scenario_fullpath = os.path.join(path_data_set,filename_scenario)
 
+# Subset of load buses to consider in the grid area, considering the area at the end of the main radial in the grid
+bus_i_subset = [90, 91, 92, 96]
+
+# Assumed power flow limit in MW that limit the load demand in the grid area (through line 85-86)
+P_lim = 0.637 
+
+# Maximum load demand of new load being added to the system
+P_max_new = 0.4
+
+# Which time series from the load data set that should represent the new load
+i_time_series_new_load = 90
+
+
 # %% Read pandapower network
 
 net = ppcsv.read_net_from_csv(path_data_set, baseMVA=10)
 
-# %% Set up hourly normalized load time series for a representative day 
+# %% Set up hourly normalized load time series for a representative day (task 2; this code is provided to the students)
 
 load_profiles = lp.load_profiles(filename_load_data_fullpath)
 
 # Get all the days of the year
 repr_days = list(range(1,366))
 
-# Get relative load profiles for representative days mapped to buses of the CINELDI reference grid;
+# Get relative load profiles for representative days mapped to buses of the CINELDI test network;
 # the column index is the bus number (1-indexed) and the row index is the hour of the year (0-indexed)
-profiles_mapped = load_profiles.map_rel_load_profiles(filename_load_data_fullpath,filename_scenario_fullpath,filename_load_mapping_fullpath,None,repr_days)
+profiles_mapped = load_profiles.map_rel_load_profiles(filename_load_mapping_fullpath,repr_days)
 
-# %% Calculate load time series in units MW (or, equivalently, MWh/h)
+# Retrieve load time series for new load to be added to the area
+new_load_profiles = load_profiles.get_profile_days(repr_days)
+new_load_time_series = new_load_profiles[i_time_series_new_load]*P_max_new
 
-# Scale the normalized load time series by the peak load value for the load points in the grid data set (in units MW);
-# the column index is the bus number (1-indexed) and the row index is the hour of the year (0-indexed)
+# Calculate load time series in units MW (or, equivalently, MWh/h) by scaling the normalized load time series by the
+# maximum load value for each of the load points in the grid data set (in units MW); the column index is the bus number
+# (1-indexed) and the row index is the hour of the year (0-indexed)
 load_time_series_mapped = profiles_mapped.mul(net.load['p_mw'])
+# %%
