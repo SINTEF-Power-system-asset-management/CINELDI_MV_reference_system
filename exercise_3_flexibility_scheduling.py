23ca481acefa6ab49f21e7f5cdf0702c8c88d25c
Iver Bakken Sperstad <iver.bakken.sperstad@sintef.no>
2024-10-02 10:54:18 +0200

    Update Exercise 3 intro script and data for 2024

--- exercise_3_flexibility_scheduling.py
+++ exercise_3_flexibility_scheduling.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Mar 31 15:30:27 2023
-@author: merkebud
-"""
-#REFERENCES
-#https://pyomo.readthedocs.io/en/stable/contributed_packages/mindtpy.html
-#https://coin-or.github.io/Ipopt/index.html
 
+@author: merkebud, ivespe
+
+Intro script for Exercise 3 ("Scheduling flexibility resources") 
+in specialization course module "Flexibility in power grid operation and planning" 
+at NTNU (TET4565/TET4575) 
+
+"""
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -15,23 +18,27 @@ from pyomo.core import Var
 import pyomo.environ as en
 import time
 
-
-#Read battery specification 
+#%% Read battery specifications
 parametersinput = pd.read_csv('./battery_data.csv', index_col=0)
 parameters = parametersinput.loc[1]
 
-#Battery Specification
-capacity=parameters['Battery_Capacity']
-charging_power_limit=parameters["Battery_Power"]
-discharging_power_limit=parameters["Battery_Power"]
-charging_efficiency=parameters["Battery_Charge_Efficiency"]
-discharging_efficiency=parameters["Battery_Discharge_Efficiency"]
-
-# Read load and PV profile data
+#Parse battery specification
+capacity=parameters['Energy_capacity']
+charging_power_limit=parameters["Power_capacity"]
+discharging_power_limit=parameters["Power_capacity"]
+charging_efficiency=parameters["Charging_efficiency"]
+discharging_efficiency=parameters["Discharging_efficiency"]
+#%% Read load demand and PV production profile data
 testData = pd.read_csv('./profile_input.csv')
 
 # Convert the various timeseries/profiles to numpy arrays
-load1 = testData['Base_Load'].values
-PV1 = testData['PV'].values
-sellPrice = testData['Feed_Price'].values
-buyPrice = testData['Load_Price'].values
+Hours = testData['Hours'].values
+Base_load = testData['Base_load'].values
+PV_prod = testData['PV_prod'].values
+Price = testData['Price'].values
+
+# Make dictionaries (for simpler use in Pyomo)
+dict_Prices = dict(zip(Hours, Price))
+dict_Base_load = dict(zip(Hours, Base_load))
+dict_PV_prod = dict(zip(Hours, PV_prod))
+# %%
