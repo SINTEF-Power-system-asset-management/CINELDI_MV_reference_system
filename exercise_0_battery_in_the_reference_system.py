11e09a2ac0377f4ce8aaf7c6f15d3ee722e791ff
Iver Bakken Sperstad <iver.bakken.sperstad@sintef.no>
2026-09-05 18:50:08 +0200

    Make Jupyter notebook for exercise 0

--- exercise_0_battery_in_the_reference_system.py
+++ exercise_0_battery_in_the_reference_system.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
-"""
-Created on 2023-07-13
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: title,-all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.5
+#   kernelspec:
+#     display_name: cineldi-mv-reference-system (3.14.6.final.0)
+#     language: python
+#     name: python3
+# ---
 
-@author: ivespe
+# %% [markdown]
+# # Script for warm-up exercise ("exercise 0"): A battery in the grid
+#
+# Intro script for warm-up exercise ("exercise 0") in specialization course module 
+# "Flexibility in power grid operation and planning" at NTNU (TET4565/TET4575) 
 
-Intro script for warm-up exercise ("exercise 0") in specialization course module 
-"Flexibility in power grid operation and planning" at NTNU (TET4565/TET4575) 
-"""
-
-# %% Dependencies
+# %%
+# Dependencies
 
 import pandapower as pp
 import pandapower.plotting as pp_plotting
@@ -17,11 +31,9 @@ import os
 import load_scenarios as ls
 import load_profiles as lp
 import pandapower_read_csv as ppcsv
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import math
 
-# %% Define input data
+# %%
+# Define input data
 
 # Location of (processed) data set for CINELDI MV reference system
 # (to be replaced by your own local data folder)
@@ -32,18 +44,21 @@ filename_irregular_fullpath = os.path.join(path_data_set,'time_series_IDs_irregu
 filename_load_data_fullpath = os.path.join(path_data_set,'load_data_CINELDI_MV_reference_system.csv')
 filename_load_mapping_fullpath = os.path.join(path_data_set,'mapping_loads_to_CINELDI_MV_reference_grid.csv')
 
-# %% Read pandapower network
+# %%
+# Read pandapower network
 
 net = ppcsv.read_net_from_csv(path_data_set, baseMVA=10)
 
-# %% Test running power flow with a peak load model
+# %%
+# Test running power flow with a peak load model
 # (i.e., all loads are assumed to be at their annual peak load simultaneously)
 
 pp.runpp(net,init='results',algorithm='bfsw')
 
 print('Total load demand in the system assuming a peak load model: ' + str(net.res_load['p_mw'].sum()) + ' MW')
 
-# %% Plot results of power flow calculations
+# %%
+# Plot results of power flow calculations
 
 pp_plotting.pf_res_plotly(net)
 
