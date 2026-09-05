0b681eca66612df711215a1a0c5def2e0f4db8b1
Iver Bakken Sperstad <iver.bakken.sperstad@sintef.no>
2026-09-05 18:10:31 +0200

    Remove script for Exercise 3, to be replaced

---
+++ exercise_3_risk-based.py
@@ -0,0 +1,11 @@
+"""
+Created on 2026-09-05
+
+@author: ivespe
+
+Intro script for Exercise 3 ("Risk-based grid operation") in specialization course module 
+"Flexibility in power grid operation and planning" at NTNU (TET4565/TET4575) 
+
+"""
+
+# TODO: Complete code
